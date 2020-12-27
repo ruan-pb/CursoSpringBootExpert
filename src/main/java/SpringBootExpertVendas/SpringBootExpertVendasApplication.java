2b81e33b20ccc364286f5bc18c833333e9561a93
Ruan-_pb <ruancruzsoares@hotmail.com>
2020-12-27 10:46:47 -0300

    salvando e recuperando cliente com Querys

--- src/main/java/SpringBootExpertVendas/SpringBootExpertVendasApplication.java
+++ src/main/java/SpringBootExpertVendas/SpringBootExpertVendasApplication.java
@@ -1,47 +1,60 @@
 package SpringBootExpertVendas;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import SpringBootExpertVendas.interfaceAnimal.Animal;
+import SpringBootExpertVendas.domain.Cliente;
+import SpringBootExpertVendas.domain.repository.ClienteRepository;
 
 @SpringBootApplication
 @RestController
 public class SpringBootExpertVendasApplication {
 	
-	//adicionar um valor/objeto externo na variavel escolhida
-	//@Value("${application.name}") = para pegar o valor do arquivo application.properties
-	
-	@Autowired
-	@Qualifier("applicationName")
-	private String applicationName;
-	
-	
-	@Autowired
-	@Qualifier("cachorro")//ou gato
-	public Animal animal;
-	
-	
-	@Bean
-	public CommandLineRunner executar() {
-		return args ->{
-			this.animal.fazerBarulho();
-		};
-	}
+	 @Bean
+	    public CommandLineRunner init(@Autowired ClienteRepository clientes){
+	        return args -> {
+	            System.out.println("Salvando clientes");
+	            clientes.salvar(new Cliente("Dougllas"));
+	            clientes.salvar(new Cliente("Outro Cliente"));
+
+	            List<Cliente> todosClientes = clientes.obterTodos();
+	            todosClientes.forEach(System.out::println);
+
+	            System.out.println("Atualizando clientes");
+	            todosClientes.forEach(c -> {
+	                c.setNome(c.getNome() + " atualizado.");
+	                clientes.atualizar(c);
+	            });
+
+	            todosClientes = clientes.obterTodos();
+	            todosClientes.forEach(System.out::println);
+
+	            System.out.println("Buscando clientes");
+	            clientes.buscarPorNome("Cli").forEach(System.out::println);
+
+//	            System.out.println("deletando clientes");
+//	            clientes.obterTodos().forEach(c -> {
+//	                clientes.deletar(c);
+//	            });
+
+	            todosClientes = clientes.obterTodos();
+	            if(todosClientes.isEmpty()){
+	                System.out.println("Nenhum cliente encontrado.");
+	            }else{
+	                todosClientes.forEach(System.out::println);
+	            }
+	        };
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExpertVendasApplication.class, args);
 	}
 	
-	@GetMapping("/heloo")
-	public String helloWord() {
-		return applicationName;
-	}
 
 }
