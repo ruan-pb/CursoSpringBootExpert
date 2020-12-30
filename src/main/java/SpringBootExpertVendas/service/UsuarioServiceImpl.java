250bf53a6d325c7732f3bf152c3620188a4702a3
Ruan-_pb <ruancruzsoares@hotmail.com>
2020-12-30 15:44:12 -0300

    Configurando as roles e Authorities

---
+++ src/main/java/SpringBootExpertVendas/service/UsuarioServiceImpl.java
@@ -0,0 +1,18 @@
+package SpringBootExpertVendas.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UsuarioServiceImpl implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return null;
+	}
+	
+
+}
