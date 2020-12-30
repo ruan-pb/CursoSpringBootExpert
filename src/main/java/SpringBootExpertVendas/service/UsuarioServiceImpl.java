83f62810a011f9aabd274eb0dc0cb62f2a122f8f
Ruan-_pb <ruancruzsoares@hotmail.com>
2020-12-30 16:00:07 -0300

    implementação do UserDetailsService

--- src/main/java/SpringBootExpertVendas/service/UsuarioServiceImpl.java
+++ src/main/java/SpringBootExpertVendas/service/UsuarioServiceImpl.java
@@ -1,17 +1,32 @@
 package SpringBootExpertVendas.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class UsuarioServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private PasswordEncoder encoder;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return null;
+		if(!username.equals("cicrano")) {
+			throw new UsernameNotFoundException("Usuario não encontrado");
+		}
+		
+		return User.builder()
+				.username("cicrano")
+				.password(encoder.encode("123"))
+				.roles("USER","ADMIN")
+				.build();
+				
 	}
 	
 
